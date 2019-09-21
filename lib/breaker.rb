9a5a9abd89648f0a3e54774d5314afdebddfa2ad
Guy Calloway <[guyfcalloway@gmail.com]>
2019-09-21 16:58:26 +0100

    Refactor

--- lib/breaker.rb
+++ lib/breaker.rb
@@ -1,13 +1,9 @@
 def breaker(string)
-    if string == ""
-       return string
-    else
-       index_array = []
-       string.each_char.with_index { |x, i| if x.downcase != x
-                                              index_array << i
-                                           end
-    }
+    index_array = []
+    string.each_char.with_index { |x, i| if x.downcase != x
+                                            index_array << i
+                                        end
+                                }
     index_array.reverse.each{ |i| string.insert(i, " ") }
     return string
-    end
 end
